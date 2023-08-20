a266de622c91812c8f20ce10c29dacba78fce7a6
Matthew Yang <myang98732190@gmail.com>
2023-08-19 23:35:10 -0700

    Updated options page

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pygame
 from main_menu import MainMenuScreen
 from game_screen import GameScreen
 from screen_manager import ScreenManager
+from options import Options
 
 
 if __name__ == "__main__":
     screen_manager = ScreenManager()
     game_screen = GameScreen()
     main_menu_screen = MainMenuScreen()
+    options_screen = Options()
     
-    screen_manager.set_screen(main_menu_screen)
+    screen_manager.set_screen(options_screen)
     screen_manager.run()
--- options.py
+++ options.py
@@ -1,11 +1,11 @@
 import pygame
 import sys
 
-class options:
+class Options:
     def __init__(self):
         pygame.init()
         
-        self.screen_color = (0, 212, 255)
+        self.screen_color = (0, 166, 198)
         self.l_blue = (196, 245, 255)
         self.d_blue = (85, 227, 255)
         self.dropdown_color = (128, 128, 128)
@@ -19,8 +19,10 @@ class options:
         self.radar_off_button = pygame.Rect(800, 300, 50, 25)
         self.nukes_on_button = pygame.Rect(700, 350, 50, 25)
         self.nukes_off_button = pygame.Rect(800, 350, 50, 25)
-
+        
         self.clock = pygame.time.Clock()
+        self.labels = ["Difficulty", "Torpedoes", "Radar", "Nukes"]
+        self.label_positions = [(50, 100), (50, 200), (50, 300), (50, 400)]
         self.dropdown_options = ["Easy", "Medium", "Hard", "Not Fair"]
         self.selected_difficulty = None
         self.dropdown_visible = False
@@ -38,21 +40,33 @@ class options:
                 if self.torpedoes_on_button.collidepoint(event.pos):
                     if not self.torpedoes_on:
                         self.torpedoes_on = True
+                        self.torpedoes_on_button.fill(self.d_blue)
+                        self.torpedoes_off_button.fill(self.l_blue)
                 if self.torpedoes_off_button.collidepoint(event.pos):
                     if self.torpedoes_on:
                         self.torpedoes_on = False
+                        self.torpedoes_off_button.fill(self.d_blue)
+                        self.torpedoes_on_button.fill(self.l_blue)
                 if self.radar_on_button.collidepoint(event.pos):
                     if not self.radar_on:
                         self.radar_on = True
+                        self.radar_on_button.fill(self.d_blue)
+                        self.radar_off_button.fill(self.l_blue)
                 if self.radar_off_button.collidepoint(event.pos):
                     if self.radar_on:
                         self.radar_on = False
+                        self.radar_off_button.fill(self.d_blue)
+                        self.radar_on_button.fill(self.l_blue)
                 if self.nukes_on_button.collidepoint(event.pos):
                     if not self.nukes_on:
                         self.nukes_on = True
+                        self.nukes_on_button.fill(self.d_blue)
+                        self.nukes_off_button.fill(self.l_blue)
                 if self.nukes_off_button.collidepoint(event.pos):
                     if self.nukes_on:
                         self.nukes_on = False
+                        self.nukes_off_button.fill(self.d_blue)
+                        self.nukes_on_button.fill(self.l_blue)
                 
                 if self.dropdown_button.collidepoint(event.pos):
                     self.dropdown_visible = not self.dropdown_visible
@@ -66,70 +80,20 @@ class options:
                         
                         
     def update(self):
-        pass
+        pygame.display.flip()
+    
+    
     
     def draw(self, surface):
         surface.fill(self.screen_color)
         
+        pygame.draw.rect(surface, self.d_blue, self.torpedoes_on_button)
+        pygame.draw.rect(surface, self.l_blue, self.torpedoes_off_button)
+        pygame.draw.rect(surface, self.d_blue, self.radar_on_button)
+        pygame.draw.rect(surface, self.l_blue, self.radar_off_button)
+        pygame.draw.rect(surface, self.d_blue, self.nukes_on_button)
+        pygame.draw.rect(surface, self.l_blue, self.nukes_off_button)
         
         
         
-    
-
-def draw_text(text, x, y, color=BLACK):
-    text_surface = font.render(text, True, color)
-    screen.blit(text_surface, (x, y))
-
-def draw_button(x, y, text, highlighted=False):
-    button_color = WHITE if not highlighted else GRAY
-    pygame.draw.rect(screen, button_color, (x, y, BUTTON_WIDTH, BUTTON_HEIGHT))
-    draw_text(text, x + 10, y + 10, BLACK)
-
-def draw_switch(x, y, text, is_on=False):
-    switch_color = (0, 255, 0) if is_on else (255, 0, 0)
-    pygame.draw.rect(screen, switch_color, (x, y, SWITCH_WIDTH, SWITCH_HEIGHT))
-    draw_text(text, x + 100, y + 10, BLACK)
-
-def main():
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                # Check for button clicks
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-
-                if BUTTON_SPACING <= mouse_y <= BUTTON_SPACING + BUTTON_HEIGHT:
-                    if 50 <= mouse_x <= 50 + BUTTON_WIDTH:
-                        selected_button = "easy"
-                    elif 300 <= mouse_x <= 300 + BUTTON_WIDTH:
-                        selected_button = "medium"
-                    elif 550 <= mouse_x <= 550 + BUTTON_WIDTH:
-                        selected_button = "hard"
-                else:
-                    selected_button = None
-
-        # Draw the options screen
-        screen.fill(WHITE)
-
-        # Draw buttons
-        draw_button(50, BUTTON_SPACING, "Easy", selected_button == "easy")
-        draw_button(300, BUTTON_SPACING, "Medium", selected_button == "medium")
-        draw_button(550, BUTTON_SPACING, "Hard", selected_button == "hard")
-
-        # Draw switches
-        draw_switch(100, SWITCH_SPACING, "Torpedoes", torpedoes_on)
-        draw_switch(300, SWITCH_SPACING, "Radar", radar_on)
-        draw_switch(500, SWITCH_SPACING, "Nukes", nukes_on)
-
-        # Draw "Back" button
-        draw_button(350, HEIGHT - 100, "Back")
-
-        pygame.display.flip()
-        clock.tick(30)
-
-if __name__ == "__main__":
-    main()
+        
