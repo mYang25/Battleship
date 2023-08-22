f656b63dd4994531e96654fb63023c78ee5363c5
Matthew Yang <myang98732190@gmail.com>
2023-08-21 17:48:16 -0700

    Finished most of the infrastructure of the setting.py page

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pygame
 from main_menu import MainMenuScreen
 from game_screen import GameScreen
 from screen_manager import ScreenManager
-from options import Options
+from settings import settings
 
 
 if __name__ == "__main__":
     screen_manager = ScreenManager()
     game_screen = GameScreen()
     main_menu_screen = MainMenuScreen()
-    options_screen = Options()
+    settings_screen = settings()
     
-    screen_manager.set_screen(options_screen)
+    screen_manager.set_screen(settings_screen)
     screen_manager.run()
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-class Options:
+class settings:
     def __init__(self):
         pygame.init()
         
@@ -9,26 +9,32 @@ class Options:
         self.l_blue = (196, 245, 255)
         self.d_blue = (85, 227, 255)
         self.dropdown_color = (128, 128, 128)
+        self.text_color = (255, 255, 255)
         
-        self.font = pygame.font.Font(None, 40)
+        self.title_font = pygame.font.Font(None, 64)
         
-        self.dropdown_menu = pygame.Rect(700, 200, 200, 25)
-        self.torpedoes_on_button = pygame.Rect(700, 250, 50, 25)
-        self.torpedoes_off_button = pygame.Rect(800, 250, 50, 25)
-        self.radar_on_button = pygame.Rect(700, 300, 50, 25)
-        self.radar_off_button = pygame.Rect(800, 300, 50, 25)
-        self.nukes_on_button = pygame.Rect(700, 350, 50, 25)
-        self.nukes_off_button = pygame.Rect(800, 350, 50, 25)
-        
-        self.clock = pygame.time.Clock()
+        self.label_font = pygame.font.Font(None, 40)
         self.labels = ["Difficulty", "Torpedoes", "Radar", "Nukes"]
-        self.label_positions = [(50, 100), (50, 200), (50, 300), (50, 400)]
+        self.label_positions = [(150, 125), (150, 225), (150, 325), (150, 425)]
+        
+        self.back_button = pygame.Rect(25, 25, 100, 50)
+              
+        self.dropdown_menu = pygame.Rect(700, 100, 200, 75)
         self.dropdown_options = ["Easy", "Medium", "Hard", "Not Fair"]
         self.selected_difficulty = None
         self.dropdown_visible = False
-        self.torpedoes_on = True
-        self.radar_on = True
+               
+        self.torpedoes_on_button = pygame.Rect(700, 200, 100, 75)
+        self.torpedoes_off_button = pygame.Rect(800, 200, 100, 75)
+        self.torpedoes_on = True        
+        self.radar_on_button = pygame.Rect(700, 300, 100, 75)
+        self.radar_off_button = pygame.Rect(800, 300, 100, 75)
+        self.radar_on = True        
+        self.nukes_on_button = pygame.Rect(700, 400, 100, 75)
+        self.nukes_off_button = pygame.Rect(800, 400, 100, 75)
         self.nukes_on = True
+                
+        self.clock = pygame.time.Clock()
         
         
         
@@ -40,35 +46,23 @@ class Options:
                 if self.torpedoes_on_button.collidepoint(event.pos):
                     if not self.torpedoes_on:
                         self.torpedoes_on = True
-                        self.torpedoes_on_button.fill(self.d_blue)
-                        self.torpedoes_off_button.fill(self.l_blue)
                 if self.torpedoes_off_button.collidepoint(event.pos):
                     if self.torpedoes_on:
                         self.torpedoes_on = False
-                        self.torpedoes_off_button.fill(self.d_blue)
-                        self.torpedoes_on_button.fill(self.l_blue)
                 if self.radar_on_button.collidepoint(event.pos):
                     if not self.radar_on:
                         self.radar_on = True
-                        self.radar_on_button.fill(self.d_blue)
-                        self.radar_off_button.fill(self.l_blue)
                 if self.radar_off_button.collidepoint(event.pos):
                     if self.radar_on:
                         self.radar_on = False
-                        self.radar_off_button.fill(self.d_blue)
-                        self.radar_on_button.fill(self.l_blue)
                 if self.nukes_on_button.collidepoint(event.pos):
                     if not self.nukes_on:
                         self.nukes_on = True
-                        self.nukes_on_button.fill(self.d_blue)
-                        self.nukes_off_button.fill(self.l_blue)
                 if self.nukes_off_button.collidepoint(event.pos):
                     if self.nukes_on:
                         self.nukes_on = False
-                        self.nukes_off_button.fill(self.d_blue)
-                        self.nukes_on_button.fill(self.l_blue)
                 
-                if self.dropdown_button.collidepoint(event.pos):
+                if self.dropdown_menu.collidepoint(event.pos):
                     self.dropdown_visible = not self.dropdown_visible
                 elif self.dropdown_visible:
                     for i, option in enumerate(self.dropdown_options):
@@ -96,4 +90,18 @@ class Options:
         
         
         
+        for label, position in zip(self.labels, self.label_positions):
+            text_surface = self.label_font.render(label, True, self.text_color)
+            text_rect = text_surface.get_rect()
+            text_rect.topleft = position
+            surface.blit(text_surface, text_rect)
+            
+        title_surface = self.title_font.render("Settings", True, self.text_color)
+        title_rect = title_surface.get_rect()
+        title_rect.topleft = (400, 30)
+        surface.blit(title_surface, title_rect)
         
+        pygame.draw.rect(surface, (150, 150, 150), self.back_button)
+        back_button_text_surface = self.label_font.render("Back", True, (0, 0, 0))
+        back_button_text_rect = back_button_text_surface.get_rect(center=self.back_button.center)
+        surface.blit(back_button_text_surface, back_button_text_rect)    
